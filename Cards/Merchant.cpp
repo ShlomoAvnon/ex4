39cbb8ec97ebc2a5f5b1e6fcb577975ca09209e8
teich <roi.teichman@campus.technion.ac.il>
2022-06-21 17:50:55 +0300

    handel max_int in Merchant.cpp

--- Cards/Merchant.cpp
+++ Cards/Merchant.cpp
@@ -39,7 +39,7 @@ void Merchant::applyEncounter(Player &player) const {
     while (true) {
         getline(cin, strChoice, LINEBREAK);
         flag = checkNumber(strChoice);
-        if (flag) {
+        if (flag && strChoice.length()<2) {
             numChoice = std::stoi(strChoice);
             if (numChoice < NUM_OF_CHOICES && numChoice >= 0) {
                 flag = true;
